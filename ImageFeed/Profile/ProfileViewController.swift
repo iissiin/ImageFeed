422122029d638d66125c1ad29a3ad6312605660f
iissiin <issina080208@gmail.com>
2025-05-26 22:35:27 +0500

    Верстка профиля

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+    
+}
